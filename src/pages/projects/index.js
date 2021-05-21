af15d541933bc8428d50df356ff3788afd5b2820
z-sohaib <js_zouambia@esi.dz>
2021-05-21 19:54:37 +0100

    Featured Images

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
+import Img from 'gatsby-image'
 
 export default function Projects({ data }) {
     console.log(data)
@@ -18,6 +19,7 @@ export default function Projects({ data }) {
                     {projects.map(project => (
                         <Link to={"/projects" + project.frontmatter.slug} key={project.id}>
                             <div>
+                                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
                                 <h3>{project.frontmatter.title}</h3>
                                 <p>{project.frontmatter.stack}</p>
                             </div>
@@ -35,20 +37,27 @@ export default function Projects({ data }) {
 export const query = graphql`
 query ProjectsPage {
     projects: allMarkdownRemark(sort: {fields: frontmatter___date, order: ASC}) {
-        nodes {
-          html
-          frontmatter {
-            title
-            stack
-            slug
+      nodes {
+        html
+        frontmatter {
+          title
+          stack
+          slug
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
           }
-          id
         }
+        id
+      }
     }
     contact: site {
-        siteMetadata {
-            contact
-        }
+      siteMetadata {
+        contact
+      }
     }
   }
 `
