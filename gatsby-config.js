4aae3f0a5dafd4c37c3474b8fb9d39d17b7b06e6
z-sohaib <js_zouambia@esi.dz>
2021-05-21 14:20:45 +0100

    site metadata & page queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: 'Zombo',
+    description: 'web dev portfolio',
+    copyright: 'This website is copyright 2021 Zombo'
+  }
 }
