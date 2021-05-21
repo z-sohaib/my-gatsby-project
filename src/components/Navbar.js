187d4c2970f61b80bad5e997584d31ad2279a68b
z-sohaib <js_zouambia@esi.dz>
2021-05-21 11:24:07 +0100

    Styling Components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Navbar() {
     return (
         <nav>
-            <h1>Web Warrior</h1>
+            <h1>Zombo</h1>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
