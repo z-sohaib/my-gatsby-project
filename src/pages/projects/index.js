f1e981745c232f547d1cf59c71bf49ffe8d3ecb8
z-sohaib <js_zouambia@esi.dz>
2021-05-21 20:44:18 +0100

    Generating Pages

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,48 +1,47 @@
-import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
-import Img from 'gatsby-image'
+import { Link, graphql } from 'gatsby'
+import Img from "gatsby-image"
 
-export default function Projects({ data }) {
-    console.log(data)
-    const projects = data.projects.nodes
-    const contact = data.contact.siteMetadata.contact
+const Projects = ({ data }) => {
+  console.log(data)
+  const projects = data.projects.nodes
+  const contact = data.contact.siteMetadata.contact
 
-    return (
-        <Layout>
-            Hello
-            <div className={styles.portfolio}>
-                <h2>Portfolio</h2>
-                <h3>Projects & Websites I've Created</h3>
-                <div className={styles.projects}> 
-                    {projects.map(project => (
-                        <Link to={"/projects" + project.frontmatter.slug} key={project.id}>
-                            <div>
-                                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
-                                <h3>{project.frontmatter.title}</h3>
-                                <p>{project.frontmatter.stack}</p>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-                <p>Like what you see? Email me at { contact } for a quote</p>
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <div className={styles.portfolio}>
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've Created</h3>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+              <div>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
+                <h3>{ project.frontmatter.title }</h3>
+                <p>{ project.frontmatter.stack }</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+        <p>Likewhat you see? email me at {contact} for a quote!</p>
+      </div>
+    </Layout>
+  );
 }
-
+ 
+export default Projects
 
 // export page query
 export const query = graphql`
-query ProjectsPage {
-    projects: allMarkdownRemark(sort: {fields: frontmatter___date, order: ASC}) {
+  query ProjectsPage {
+    projects: allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}) {
       nodes {
-        html
         frontmatter {
-          title
-          stack
           slug
+          stack
+          title
           thumb {
             childImageSharp {
               fluid {
--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import Img from 'gatsby-image'
+import React from "react"
+// import Img from "gatsby-image"
+import Layout from "../components/Layout"
 import * as styles from '../styles/project-details.module.css'
 
-export default function ProjectDetails() {
-    return (
-        <Layout>
-            <div className={styles.details}>
-                <h2>title</h2>
-                <h3>stack</h3>
-                <div className={styles.featured}>
-                    <Img fluid={} />
-                </div>
-                <div className={styles.html} dangerouslySetInnerHTML={} />
-            </div>
-        </Layout>
-    )
+const ProjectDetails = () => {
+  return (
+    <Layout>
+      <div className={styles.details}>
+        <h2>title</h2>
+        <h3>stack</h3>
+        <div className={styles.featured}>
+          {/* <Img fluid={} /> */}
+        </div>
+        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+      </div>
+    </Layout>
+  )
 }
+ 
+export default ProjectDetails
